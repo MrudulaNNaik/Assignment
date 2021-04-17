7d1a3fbc45aa5402b027eb426432bc63333cd643
MRUDULA N NAIK <mrudulannaik1999@gmail.com>
2021-04-18 02:09:18 +0530

    palindrome

---
+++ Assignments/src/FirstAssignment/StringPalindrome.java
@@ -0,0 +1,18 @@
+package FirstAssignment;
+
+import java.util.Scanner;
+
+public class StringPalindrome {
+	 public static void main(String args[])  
+	   {  
+	     String s1 = "naik";
+	     String s2 = "";	    		 
+	      int length = s1.length();   
+	      for ( int i = length - 1; i >= 0; i-- )  
+	         s2 = s2 + s1.charAt(i);  
+	      if (s1.equals(s2))  
+	         System.out.println(" is a palindrome");  
+	      else  
+	         System.out.println(" is not a palindrome");   
+	   }  
+}
